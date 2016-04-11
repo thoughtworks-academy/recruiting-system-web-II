2fe625fb27523153c82ef3b2e414c5e4b740d030
wengjiaojiao <419909058@qq.com>
2016-04-11 15:48:16 +0800

    rename the school name

--- source/scripts/component/navigation/navigation.component.jsx
+++ source/scripts/component/navigation/navigation.component.jsx
@@ -5,7 +5,7 @@ var Navigation = React.createClass({
     return (
         <nav>
           <div className="brand">
-            <a href="/"><strong>思沃特训营</strong></a>
+            <a href="/"><strong>校招特训营</strong></a>
           </div>
           {this.props.children}
         </nav>
