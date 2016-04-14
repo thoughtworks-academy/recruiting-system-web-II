35b3a0c290e1207086330197068ab692869d2550
purpletianjing <TianjingSun@outlook.com>
2016-04-14 18:02:38 +0800

    delete addPaper

--- source/scripts/component/style-guide/add-paper.component.jsx
+++ source/scripts/component/style-guide/add-paper.component.jsx
@@ -1,9 +1,6 @@
 'use strict';
 
 var AddPaper = React.createClass({
-  addPaper: function() {
-    this.pros.addPaper();
-  },
   render: function() {
     return (
       <div className="paper-button col-xs-12 text-center">
