f489c7ade28ee4dadab8a74c0c19d502f9e034db
purpletianjing <TianjingSun@outlook.com>
2016-04-14 17:52:15 +0800

    delete unuseful onclick

--- source/scripts/component/style-guide/add-paper.component.jsx
+++ source/scripts/component/style-guide/add-paper.component.jsx
@@ -6,7 +6,7 @@ var AddPaper = React.createClass({
   },
   render: function() {
     return (
-      <div className="paper-button col-xs-12 text-center"><button onClick={}></button>
+      <div className="paper-button col-xs-12 text-center">
         <i className="fa fa-plus text-primary fa-5x"></i>
       </div>
     )
