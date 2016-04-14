31b0f6bd01bf1ec4a35ba3c652bacefe4d88d97d
purpletianjing <TianjingSun@outlook.com>
2016-04-14 09:37:08 +0800

    add paper

--- source/scripts/component/style-guide/add-paper.component.jsx
+++ source/scripts/component/style-guide/add-paper.component.jsx
@@ -1,9 +1,12 @@
 'use strict';
 
 var AddPaper = React.createClass({
+  addPaper: function() {
+    this.pros.addPaper();
+  },
   render: function() {
     return (
-      <div className="paper-button col-xs-12 text-center">
+      <div className="paper-button col-xs-12 text-center"><button onClick={}></button>
         <i className="fa fa-plus text-primary fa-5x"></i>
       </div>
     )
